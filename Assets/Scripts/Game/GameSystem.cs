de6989bc6f67f9d5df95b7a3d36fa0de4561d9cb
Tomasz Kierpiec <kierpiec.tomek@gmail.com>
2024-03-08 10:52:25 +0100

    Inputs + scene changes

---
+++ Assets/Scripts/Game/GameSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using Unity.Burst;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Game {
+	public partial struct GameSystem : ISystem {
+
+		public float GameTime;
+
+		public void OnUpdate(ref SystemState state) {
+			GameTime += SystemAPI.Time.DeltaTime;
+		}
+	}
+}
